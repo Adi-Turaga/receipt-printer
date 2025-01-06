c7b8d16296f81187df6b67f24bc82e9e175a0029
Adi Turaga <adi.s.turaga@gmail.com>
2025-01-06 15:09:14 -0500

    Added printer functionality to app.py

--- app.py
+++ app.py
@@ -2,7 +2,23 @@ from flask import Flask, render_template, request
 from werkzeug.utils import secure_filename
 from flask_cors import CORS
 import os
-import json
+from escpos import *
+from PIL import Image
+
+DESIRED_WIDTH = 400
+
+p = printer.Usb(0x28e9, 0x0289, 0, in_ep=0x81, out_ep=0x03)
+p.open()
+
+def resize_image(img_path):
+    image = Image.open(img_path)
+    width, height = image.size
+
+    new_width = DESIRED_WIDTH
+    new_height = int(height * (DESIRED_WIDTH/width))
+
+    image = image.resize((new_width, new_height))
+    return image
 
 app  = Flask(__name__)
 CORS(app)
@@ -16,6 +32,9 @@ def index():
         if request.is_json == True:
             data = request.get_json()
             print("{}: {}".format(data['type'], data['data']))
+            if data['type'] == 'text': p.text(data['data'])
+            elif data['type'] == 'qr': p.qr(data['data'], size=12)
+            p.cut()
             return "<h1>Successfully entered text</h1>"
         else:
             file = request.files['img']
@@ -23,6 +42,8 @@ def index():
             print(filename)
             img = os.path.join(app.config['UPLOAD'], filename)
             file.save(img)
+            p.image(resize_image(img))
+            p.cut()
     return render_template('index.html')
 
 if __name__ == "__main__":
